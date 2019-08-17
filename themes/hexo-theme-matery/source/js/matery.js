0804a9e41f8d9be83281bbf01873847186e1b8b1
ctwang <ctwang1994@163.com>
2019-08-18 03:27:42 +0800

    主题个性定制完成，todo必要的话整理一篇博客或者更新说明

--- themes/hexo-theme-matery/source/js/matery.js
+++ themes/hexo-theme-matery/source/js/matery.js
@@ -130,7 +130,7 @@ $(function () {
     $(window).scroll(function () {
         /* 回到顶部按钮根据滚动条的位置的显示和隐藏.*/
         let scroll = $(window).scrollTop();
-        if (scroll < 100) {
+        if (scroll < 360) {
             $nav.addClass('nav-transparent');
             $backTop.slideUp(300);
         } else {
